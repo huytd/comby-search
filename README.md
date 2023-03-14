203c071e25107d94e4cf3def3aaf0cfb78fa6be2
Huy <kingbazoka@gmail.com>
2023-03-13 23:05:02 -0700

    Add option to change display lines. Use Clap

--- README.md
+++ README.md
@@ -32,6 +32,18 @@ Or compile it from source.
 
 ## Usage
 
+```
+Usage: cb [OPTIONS] <QUERY>
+
+Arguments:
+  <QUERY>  Your code search query
+
+Options:
+  -f, --filter <PATTERN>      File filter pattern, for example: '*.ts,!*.spec.ts'
+  -l, --line-margin <MARGIN>  Number of lines to display before and after the matched search result
+  -h, --help                  Print help
+```
+
 To start a new search, run the command below. See [Comby's Documentation](https://comby.dev/docs/basic-usage) for more information about the search syntax.
 
 ```
@@ -47,5 +59,49 @@ $ cb 'struct :[A] { :[B] }'
 You can also limit the scope of the search to some specific files with:
 
 ```
-$ cb 'struct :[A] { :[B] }' '*.ts,*.java,!*.js'
+$ cb -f '*.ts,*.java,!*.js' 'struct :[A] { :[B] }'
+```
+
+By default, the matched result will be displayed with a margin of 3 lines before and after, you can change this with the `--line-margin` or `-l` option:
+
+```
+$cb -l 10 'foo'
+```
+
+## Examples
+
+Here are some example search with `cb`.
+
+1. Find all arrow functions in TSX files
+
+```
+$ cb -f '*.tsx,*.jsx' 'const :[1] = (:[2]) => { :[3] }'
+```
+
+2. Is there any call to the `superFoo` function with an empty string as argument?
+
+```
+$ cb 'superFoo(:[2] "" :[3])'
+```
+
+3. Find all for loop in all Java files, but not in test files
+
+```
+$ cb -f '!*Test.java' 'for ([:1]) { :[2] }'
+```
+
+4. Find all Rust functions that return a String
+
+```
+$ cb -f '*.rs' 'fn :[1] (:[2]) -> String'
+```
+
+5. Find Rust traits (can you tell the different?)
+
+```
+$ cb 'trait :[1] { fn :[2](:[3]); }'
+
+# and
+
+$ cb 'trait :[1] { fn :[2](:[3]) -> :[4]; }'
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,75 @@
-use std::process::Command;
+use clap::Parser;
 use colored::*;
+use std::process::Command;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    /// Your code search query
+    query: String,
+
+    /// File filter pattern, for example: '*.ts,!*.spec.ts'
+    #[arg(short, long, value_name = "PATTERN")]
+    filter: Option<String>,
+
+    /// Number of lines to display before and after the matched search result
+    #[arg(short, long, value_name = "MARGIN")]
+    line_margin: Option<usize>,
+}
 
 fn main() {
-    let mut args = std::env::args();
-    let query = args.nth(1);
-    let path = args.collect::<String>();
-    if query.is_some() {
-        if let Ok(output) = Command::new("comby")
-            .args(["-match-only", &query.unwrap(), "''", "-rg", &format!("-g '{}'", path)])
-            .output()
-        {
-            let stdout = std::str::from_utf8(&output.stdout).expect("Cannot parse stdout!");
-            for output_line in stdout.lines() {
-                let mut parts = output_line.split(':');
-                let file_name = parts.next().unwrap();
-                let at_line = parts.next().unwrap().parse::<usize>().unwrap();
-                let line_count = parts.collect::<String>().split(r#"\n"#).count();
-                let preview_from = at_line.saturating_sub(3).max(1);
-                let preview_to = at_line + line_count + 3;
-                let highlight_from = at_line;
-                let highlight_to = at_line + line_count;
-                let buf = Command::new("sed").args(["-n", &format!("{},{}p", preview_from, preview_to), file_name]).output().unwrap().stdout;
-                let preview_lines = std::str::from_utf8(&buf).unwrap().lines();
-                println!("{}", file_name.color("green"));
-                for (i, line_text) in preview_lines.enumerate() {
-                    let current_line = preview_from + i;
-                    let line_number = format!("{:>4}", current_line);
-                    if current_line >= highlight_from && current_line < highlight_to {
-                        println!("{} {} {}", line_number.color("red"), "│".dimmed(), line_text.color("red"));
-                    } else {
-                        println!("{} {} {}", line_number.dimmed(), "│".dimmed(), line_text.normal());
-                    }
+    let cli = Cli::parse();
+    let path_filter = cli.filter.unwrap_or(String::new());
+    let line_margin = cli.line_margin.unwrap_or(3);
+    if let Ok(output) = Command::new("comby")
+        .args([
+            "-match-only",
+            &cli.query,
+            "''",
+            "-rg",
+            &format!("-g '{}'", path_filter),
+        ])
+        .output()
+    {
+        let stdout = std::str::from_utf8(&output.stdout).expect("Cannot parse stdout!");
+        for output_line in stdout.lines() {
+            let mut parts = output_line.split(':');
+            let file_name = parts.next().unwrap();
+            let at_line = parts.next().unwrap().parse::<usize>().unwrap();
+            let line_count = parts.collect::<String>().split(r#"\n"#).count();
+            let preview_from = at_line.saturating_sub(line_margin).max(1);
+            let preview_to = at_line + line_count + line_margin;
+            let highlight_from = at_line;
+            let highlight_to = at_line + line_count;
+            let buf = Command::new("sed")
+                .args([
+                    "-n",
+                    &format!("{},{}p", preview_from, preview_to),
+                    file_name,
+                ])
+                .output()
+                .unwrap()
+                .stdout;
+            let preview_lines = std::str::from_utf8(&buf).unwrap().lines();
+            println!("{}", file_name.color("green"));
+            for (i, line_text) in preview_lines.enumerate() {
+                let current_line = preview_from + i;
+                let line_number = format!("{:>4}", current_line);
+                if current_line >= highlight_from && current_line < highlight_to {
+                    println!(
+                        "{} {} {}",
+                        line_number.color("red"),
+                        "│".dimmed(),
+                        line_text.color("red")
+                    );
+                } else {
+                    println!(
+                        "{} {} {}",
+                        line_number.dimmed(),
+                        "│".dimmed(),
+                        line_text.normal()
+                    );
                 }
             }
         }
-    } else {
-        println!("ERROR: Please enter your search query and file extensions!\nSyntax:\n    cb <query> [file extensions]\nFor example:\n    cb 'struct :[1] {{ :[2] }}' '*.rs'");
     }
 }
